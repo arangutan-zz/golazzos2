5cfd707966444be8dbb82234df3180d2a5d210bb
Andres Yepes <yepes07@gmail.com>
2013-09-09 11:46:25 -0500

    amigos en el estadio

--- app/controllers/partidos_controller.rb
+++ app/controllers/partidos_controller.rb
@@ -5,123 +5,137 @@ class PartidosController < ApplicationController
   before_filter :require_admin_login , :only => [:new,:edit,:update,:destroy,:create,:repartir]
 
   def index
-    @hora = Time.now - 1.day
-    @partidos = Partido.where("diapartido > ?", @hora ).order("diapartido ASC")
+  	@hora = Time.now - 1.day
+  	@partidos = Partido.where("diapartido > ?", @hora ).order("diapartido ASC")
 
-    respond_to do |format|
+  	respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @partidos }
-    end
   end
+end
 
   # GET /partidos/1
   # GET /partidos/1.json
   def show
-    @partido = Partido.includes(:bets).find(params[:id])
+  	@partido = Partido.includes(:bets).find(params[:id])
 
-    if params[:betid] && params[:userid]
-      @bet = Bet.find(params[:betid])
-      @user = User.find(params[:userid])
-    end
-    
-    respond_to do |format|
+  	if params[:betid] && params[:userid]
+  		@bet = Bet.find(params[:betid])
+  		@user = User.find(params[:userid])
+  		if current_user
+        if true #que no exista la amistad!
+          @friendship = current_user.friendships.build( friend_id: @user.id)
+          @friendshipDos = @user.friendships.build(friend_id: current_user.id)
+          if @friendship.save && @friendshipDos.save
+            #se creo la amistad exitosamente
+            flash[:notice]= "Se creo la amistad Correctamente"
+          end
+        else
+          #la amistad ya existe!!!
+          flash[:notice]= "la amistad ya existe!"
+        end
+  		end
+  	end
+
+  	respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @partido }
-    end    
-  end
+  end    
+end
 
   # GET /partidos/new
   # GET /partidos/new.json
   def new
-    @partido = Partido.new
+  	@partido = Partido.new
 
-    respond_to do |format|
+  	respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @partido }
-    end
   end
+end
 
   # GET /partidos/1/edit
   def edit
-    @partido = Partido.find(params[:id])
+  	@partido = Partido.find(params[:id])
   end
 
   # POST /partidos
   # POST /partidos.json
   def create
-    @partido = Partido.new(params[:partido])
+  	@partido = Partido.new(params[:partido])
 
-    respond_to do |format|
-      if @partido.save
-        @partido.create_activity :create, owner: current_user
-        format.html { redirect_to @partido, notice: 'Partido was successfully created.' }
-        format.json { render json: @partido, status: :created, location: @partido }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @partido.errors, status: :unprocessable_entity }
-      end
-    end
+  	respond_to do |format|
+  		if @partido.save
+  			@partido.create_activity :create, owner: current_user
+  			format.html { redirect_to @partido, notice: 'Partido was successfully created.' }
+  			format.json { render json: @partido, status: :created, location: @partido }
+  		else
+  			format.html { render action: "new" }
+  			format.json { render json: @partido.errors, status: :unprocessable_entity }
+  		end
+  	end
   end
 
   # PUT /partidos/1
   # PUT /partidos/1.json
   def update
-    @partido = Partido.find(params[:id])
+  	@partido = Partido.find(params[:id])
 
-    respond_to do |format|
-      if @partido.update_attributes(params[:partido])
-        if (@partido.terminado && @partido.cerrado)
-          @partido.create_activity :termino, owner: current_user
-        else
-          @partido.create_activity :update, owner: current_user
-        end
-        format.html { redirect_to @partido, notice: 'Partido was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @partido.errors, status: :unprocessable_entity }
-      end
-    end
+  	respond_to do |format|
+  		if @partido.update_attributes(params[:partido])
+  			if (@partido.terminado && @partido.cerrado)
+  				@partido.create_activity :termino, owner: current_user
+  			else
+  				@partido.create_activity :update, owner: current_user
+  			end
+  			format.html { redirect_to @partido, notice: 'Partido was successfully updated.' }
+  			format.json { head :no_content }
+  		else
+  			format.html { render action: "edit" }
+  			format.json { render json: @partido.errors, status: :unprocessable_entity }
+  		end
+  	end
   end
 
-  # DELETE /partidos/1
-  # DELETE /partidos/1.json
-  def destroy
-    @partido = Partido.find(params[:id])
-    @partido.destroy
+	# DELETE /partidos/1
+	# DELETE /partidos/1.json
+	def destroy
+		@partido = Partido.find(params[:id])
+		@partido.destroy
 
-    respond_to do |format|
-      format.html { redirect_to partidos_url }
-      format.json { head :no_content }
-    end
-  end
+		respond_to do |format|
+			format.html { redirect_to partidos_url }
+			format.json { head :no_content }
+		end
+	end
 
-  def repartir
-    @partido = Partido.find(params[:id])
-    if @partido.terminado && @partido.cerrado
-      @partido.repartir_la_plata
-      @partido.update_attributes(repartido: true)
-      redirect_to partido_path(@partido), notice: "Se repartio la plata !"
-    else
-      redirect_to edit_partido_path(@partido), notice: "Asegurate de Cerrar la apuesta y de agregar el marcador"
-    end
-  end
+	def repartir
+		@partido = Partido.find(params[:id])
+		if @partido.terminado && @partido.cerrado
+			@partido.repartir_la_plata
+			@partido.update_attributes(repartido: true)
+			redirect_to partido_path(@partido), notice: "Se repartio la plata !"
+		else
+			redirect_to edit_partido_path(@partido), notice: "Asegurate de Cerrar la apuesta y de agregar el marcador"
+		end
+	end
 
-  def mostrar
+	def mostrar
 
-  end
+	end
   
 
-  def retar
+	def retar
+		@partido = Partido.find(params[:id])
+		@bet = Bet.find(params[:betid])
+
+  		@linkinvitation = partido_url(@partido).to_s+"?userid=#{current_user.id}&betid=#{@bet.id}"
+    	#@linkinvitation = "http://localhost:3000"
+    	@friends = current_user.facebook.get_connections("me", "friends?fields=id,name,picture.type(square)")
+   	end
+
+	def estadio
     @partido = Partido.find(params[:id])
-    @bet = Bet.find(params[:betid])
-    
-    @linkinvitation = partido_url(@partido).to_s+"?userid=#{current_user.id}&betid=#{@bet.id}"
-    #@linkinvitation = "http://localhost:3000"
-    @friends = current_user.facebook.get_connections("me", "friends?fields=id,name,picture.type(square)")
-  end
-
-  def estadio
-
-  end
+    @following= current_user.following
+	end
 end
