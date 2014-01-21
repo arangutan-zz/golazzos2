d28b0f8de300eb199a58a2e4cc765865f545b660
Andres Yepes <andresyepes@Andress-MacBook-Air.local>
2014-01-21 15:44:16 -0500

    users_del_mes

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -2,29 +2,29 @@ class MetricsController < ApplicationController
 	before_filter :require_admin_login
 
   def index
-       @usuarios_que_apostarian            = Profile.where(:apostaria => true).count
-       @usuarios_que_apostarian_total      = Profile.where('apostaria  IS NOT NULL').count
-       @usuarios_que_apostarian_porcentaje = @usuarios_que_apostarian.to_f / @usuarios_que_apostarian_total.to_f
-       @numeroUsuarios                     = User.count
-       @numeroBets                         = Bet.all.count
-       @numeroInvitacionesPromedio         = User.average(:invitation_number)
-       @numeroUsuariosReferidos            = User.where('invitation_id  IS NOT NULL').count
-       @numeroVisitasPromedio              = User.average(:visits_number)
-       @numeroPostsPromedio                = User.average(:post_on_fb)
-       @tiempoPromedioEnAceptar            = Invitation.average(:demora)
-       @usuariosConEdad                    = User.average(:age)
-       @numeroApuestasPromedio             = User.average(:bets_number)
+       #@usuarios_que_apostarian            = Profile.where(:apostaria => true).count
+       #@usuarios_que_apostarian_total      = Profile.where('apostaria  IS NOT NULL').count
+       #@usuarios_que_apostarian_porcentaje = @usuarios_que_apostarian.to_f / @usuarios_que_apostarian_total.to_f
+       #@numeroUsuarios                     = User.count
+       #@numeroBets                         = Bet.all.count
+       #@numeroInvitacionesPromedio         = User.average(:invitation_number)
+       #@numeroUsuariosReferidos            = User.where('invitation_id  IS NOT NULL').count
+       #@numeroVisitasPromedio              = User.average(:visits_number)
+       #@numeroPostsPromedio                = User.average(:post_on_fb)
+       #@tiempoPromedioEnAceptar            = Invitation.average(:demora)
+       #@usuariosConEdad                    = User.average(:age)
+       #@numeroApuestasPromedio             = User.average(:bets_number)
 
 
        #Usuarios Activos
-       @usuarios_activos = Metrics.usuarios_activos.sort
+       #@usuarios_activos = Metrics.usuarios_activos.sort
        @porcentaje_usuarios_activos_al_mes= Metrics.porcentaje_usuarios_activos_al_mes
        
        #Recurrencia en Meses
-       @recurrencia_de_usuarios = Metrics.recurrencia_en_meses.sort
+       #@recurrencia_de_usuarios = Metrics.recurrencia_en_meses.sort
 
        #VIRALIDAD GOLAZZOS 2.0
-       @usuariosreferidos = User.sum(:referidos)
+       #@usuariosreferidos = User.sum(:referidos)
   end
   
   def emails
